61f3b67c18a7708b62dd1c3cf8ccfe69f8633ae4
Michael Stroet Laptop <michael.stroet@hotmail.com>
2019-04-03 14:53:09 +0200

    Kleur in test proteïne

--- Code/graph.py
+++ Code/graph.py
@@ -7,28 +7,41 @@ import matplotlib.pyplot as plt
 
 def draw_graph(protein):
 
+    colors = {
+        "H" : "red",
+        "P" : "blue",
+        "C" : "yellow",
+    }
+
+    L_color = []
+    L_label = []
+
     L_x = []
     L_y = []
 
     for acid in protein:
-        L_x.append(acid[0])
-        L_y.append(acid[1])
+        L_color.append(colors[acid[0]])
+        L_x.append(acid[1])
+        L_y.append(acid[2])
 
     max_distance = max([max(L_x), max(L_y)]) + 1
 
     plt.figure("Folded protein", figsize = (6, 6))
 
-    plt.plot(L_x, L_y, 'b-')
-    plt.plot(L_x, L_y, 'bo')
+    plt.plot(L_x, L_y, '-', color = "black")
+
+    for i, color in enumerate(L_color):
+        plt.plot(L_x[i], L_y[i], 'o', color = color, markersize = 10)
 
     plt.xlim([-max_distance, max_distance])
     plt.ylim([-max_distance, max_distance])
 
     plt.grid(axis = "both")
+    plt.legend(loc = "upper right")
 
     plt.show()
 
 
 if __name__ == "__main__":
-    test_protein = [[0,0], [0,1], [1,1], [2,1], [2,0], [2,-1], [1,-1], [0, -1], [-1,-1]]
+    test_protein = [["H",0,0], ["P",0,1], ["P",1,1], ["H",2,1], ["H",2,0], ["C",2,-1], ["H",1,-1], ["P",0, -1], ["H",-1,-1]]
     draw_graph(test_protein)
