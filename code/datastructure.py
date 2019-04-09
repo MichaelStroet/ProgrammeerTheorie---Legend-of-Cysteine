a7eb5c6a7129a69808eef243b20ed7a7f4294c91
Michael Stroet Laptop <michael.stroet@hotmail.com>
2019-04-09 15:50:18 +0200

    __init__ protein class

--- code/datastructure.py
+++ code/datastructure.py
@@ -2,8 +2,29 @@
 # Sophie Stiekema 10992499
 # Michael Stroet  11293284
 
-class protein:
+import numpy as np
+
+class protein():
     pass
+
+    def __init__(self, protein_length):
+        '''
+        Initialise a n x n matrix
+        '''
+        matrix_size = 2 * protein_length - 1
+
+        self.acids = np.zeros((matrix_size, matrix_size))
+
+    def __str__(self):
+        """Returns a string representation of the item"""
+
+        test = self.acids.tostring()
+        test2 = np.fromstring(test)
+        print(test)
+        print(test2)
+        print(self.acids)
+        return "nope"
+
 
 class acid:
     pass
@@ -13,3 +34,7 @@ Representatie?
     Matrix
     Coordinaten
 '''
+
+if __name__ == "__main__":
+    lol = protein(5)
+    print(lol)
