8bcd829f64fbd70d26248d2d73fc6626f7b09451
Michael Stroet PC <michael.stroet@hotmail.com>
2019-04-23 19:20:19 +0200

    fixed the actual problem in remove_acid()

--- code/datastructure.py
+++ code/datastructure.py
@@ -116,8 +116,7 @@ class Protein:
             neighbor_location = new_location(location, connection)
             neighbor_acid = self.acids[neighbor_location[0], neighbor_location[1]]
 
-            if not neighbor_acid == 0:
-                neighbor_acid.connections[key] = ""
+            neighbor_acid.connections["next"] = ""
 
         self.acids[location[0], location[1]] = 0
         self.energy = previous_energy
