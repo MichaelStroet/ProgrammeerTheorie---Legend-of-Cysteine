322d488eacb31c2e25f7932f7af566135c942438
Ruby Bron <rubybron@gmail.com>
2019-04-29 12:33:35 +0200

    removed print statements

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -25,19 +25,17 @@ def greedy(protein_string):
 	# Place the first two amino acids
 	location = [protein_length - 1, protein_length - 1]
 	protein.add_acid(protein_string[0], location, "")
-	protein.acids[location[0], location[1]].add_connection("first")
+	protein.acids[location[0], location[1]].add_connection("down")
 
 	location = [location[0] + 1, location[1]]
 	protein.add_acid(protein_string[1], location, "down")
-	protein.acids[location[0], location[1]].add_connection("up")
 
 	# Until solution is found, fold a protein greedy like
 	solution_found = False
 	while not solution_found:
 		protein_clean = copy.deepcopy(protein)
 		(solution_found, protein_result) = greedy_fold(protein_clean, protein_string, protein_length, location)
-	print(protein_result)
-	print(protein_result.energy)
+	return protein_result
 
 def greedy_fold(protein, p_string, p_len, loc_current):
 	'''
@@ -45,7 +43,6 @@ def greedy_fold(protein, p_string, p_len, loc_current):
 	and the location of the last placed amino acid
 	The ouput is a folded protein
 	'''
-	#print("line 46: protein matrix, its string, lenght and location of second placed protein\n", protein, p_string, p_len, loc_current)
 		
 	# Every direction for the following amino acid
 	for acid_index in range(2, p_len):
@@ -69,29 +66,22 @@ def greedy_fold(protein, p_string, p_len, loc_current):
 		# Check the energy of every next location
 		if len(locs_next) > 0:
 			previous_energy = protein.energy
-			#print("line 69: dict of next locations and the prev energy\n", locs_next, previous_energy)
 
 			# Every next location's energy is collected after pseudo placing
 			for direction, loc_next in locs_next.items():
 				previous_acid = protein.acids[loc_current[0], loc_current[1]]
 				previous_acid.add_connection(direction)
-				#print("line 75: direction and next location\n", direction, loc_next)
 
 				# Acid is placed, energy is stored, acid is removed again
 				protein.add_acid(acid_type, loc_next, direction)
-				# print(protein)
 				energy[direction] = protein.check_energy(loc_next, acid_type)
 				protein.remove_acid(loc_next, previous_energy)
-				#print("line85: pseudo location and type of acid\n", loc_next, acid_type)
 
 			# Compare energy, lowest else random
 			energy_mean = np.mean(list(energy.values()))
 			for key, value in energy.items():
 				if value <= energy_mean:
 					locs_possible.append(key)
-			#print("Line 92: locations", locs_possible)
-
-			#print("line 94: energy dict and energy mean\n", energy, energy_mean)
 
 			# chooses a random direction from the possible locations then adds that acid
 			loc_choice = random.choice(list(locs_possible))
@@ -104,16 +94,14 @@ def greedy_fold(protein, p_string, p_len, loc_current):
 
 			# Change the current location
 			loc_current = location
-			#print("line97: random direction of possible locations\n", loc_choice, location)
+
 		# Protein incomplete, abort folding
 		else:
 			print("line 100:\n No locations found")
 			break
-		#print("line 101: type of acid to be placed, its possible directions and energy dict\n", acid_type, "\n", directions, "\n", energy)
-	
-	# Returns the solution
+		
+	# Return the folded protein
 	if protein.length == p_len and protein.energy < 0:
 		return(True, protein)
-
 	else:
 		return(False, protein)
