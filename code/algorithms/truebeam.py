e6a938ef1f7922e560bfa7dbcb97b0be352ea64e
sophieclaire <sophie.stiekema@gmail.com>
2019-05-16 20:17:50 +0200

    small fixes BEAM <3

--- code/algorithms/truebeam.py
+++ code/algorithms/truebeam.py
@@ -17,13 +17,14 @@ from acid import Acid
 from protein import Protein
 from functions import opposite
 from functions import new_location
+from operator import itemgetter
 
 
-def beamsearch(pr_string, dimension):
+def beamsearch(pr_string, dimension, width):
 
-    global best_nodes, protein_length, protein_string, energy_counter, beam_list, proteins
+    global best_nodes, protein_length, protein_string, energy_counter, beam_list, proteins, B_width
     protein_string = pr_string
-
+    B_width = width
     # Use protein length to establish location of first amino acid in a matrix
     protein_length = len(protein_string)
     protein = Protein(protein_length, dimension)
@@ -39,15 +40,21 @@ def beamsearch(pr_string, dimension):
 
     energy_min = 0
     energy_counter = {}
-    proteins = {0:protein, 1:protein, 2:protein}
+    proteins = {}
 
-    beam_list = [0] * 3
+    for i in range(B_width):
+        proteins[i] = protein
+    #print(proteins)
+
+    beam_list = [0] * B_width
 
     next_layer(protein, previous_location)
 
-    for i in range(len(proteins)):
-        print(proteins[i].energy)
-    
+    # for i in range(len(proteins)):
+    #     print(proteins[i].energy)
+    protein_min = proteins[0]
+    #print(protein_min.energy)
+
     return protein_min, energy_counter
 
 def next_layer(the_protein, prev_location):
@@ -59,23 +66,25 @@ def next_layer(the_protein, prev_location):
 
     global protein_min
 
-    beam_possibilities = {}
     prev_locs = []
     list = []
-    temporary_proteins = [0] * 3
-    print("BEAM: ", beam_list)
+    temporary_proteins = [0] * B_width
+    #print("BEAM: ", beam_list)
     acid_type = protein_string[the_protein.length]
 
-    print("PREVIOUS LOCATION: ", prev_location)
+    #print("PREVIOUS LOCATION: ", prev_location)
+    beam_possibilities = {}
+
     for i in range(len(prev_location)):
         protein_energy ={}
         possible_sites = {}
         energy = {}
 
+
         previous_location = prev_location[i]
-        print(previous_location)
+        #print(previous_location)
         if any(proteins):
-            print(proteins[i])
+            #print(proteins[i])
             protein = proteins[i]
         else:
             protein = the_protein
@@ -109,61 +118,64 @@ def next_layer(the_protein, prev_location):
             previous_energy = protein.energy
 
             for direction, site in possible_sites.items():
+
                 acid_type = protein_string[protein.length]
 
                 previous_acid = protein.acids[previous_location[0], previous_location[1], previous_location[2]]
-                print(previous_acid)
+                #print(previous_acid)
                 previous_acid.add_connection(direction)
 
                 # Acid is placed, energy is stored, acid is removed again
                 protein.add_acid(acid_type, site, direction)
                 energy[direction] = protein.check_energy(site, acid_type)
                 total_energy = previous_energy + protein.check_energy(site, acid_type)
-                print("TOTAL energy: ", total_energy)
+                #print("TOTAL energy: ", total_energy)
                 protein_energy[direction] = total_energy
                 direction = str(i) + str(direction)
                 beam_possibilities[direction] = total_energy
                 protein.remove_acid(previous_energy)
             sorted_possibilities = sorted(beam_possibilities.items(), key=operator.itemgetter(1))
             beam_possibilities = dict(sorted_possibilities)
-            print(sorted_possibilities)
-            print(beam_possibilities)
+            #print(sorted_possibilities)
+            #print(beam_possibilities)
             list.append(beam_possibilities)
             #beam_possibilities["low"] = -1
             #beam_possibilities["high"] = -2
 
-        print("POSSIBILITIES: ",list)
+    #print("POSSIBILITIES: ",beam_possibilities)
 
     i = 0
-    for key in sorted(beam_possibilities, key=beam_possibilities.get)[:3]:
-        print(key, beam_possibilities[key])
+    #print(sorted(beam_possibilities, key=beam_possibilities.get))
+
+    for key in sorted(beam_possibilities, key=beam_possibilities.get)[:B_width]:
+        #print(key, beam_possibilities[key])
         needed_protein = int(re.findall('\d+',key)[0])
-        print(needed_protein)
+        #print(needed_protein)
         needed_direction = ''.join(filter(str.isalpha, key))
-        print(needed_direction)
+        #print(needed_direction)
         previous_loc = prev_location[needed_protein]
         needed_locations = protein.neighbors(previous_loc)
         needed_loc = needed_locations[needed_direction]
         prev_locs.append(needed_loc)
-        print(needed_protein)
-        print(beam_list[i])
+        #print(needed_protein)
+        #print(beam_list[i])
         temporary_proteins[i] = copy.deepcopy(proteins[needed_protein])
-        print(temporary_proteins[i])
+        #print(temporary_proteins[i])
         prev_acid = temporary_proteins[i].acids[previous_loc[0], previous_loc[1], previous_loc[2]]
-        print(prev_acid)
+        #print(prev_acid)
         prev_acid.add_connection(needed_direction)
 
         temporary_proteins[i].add_acid(acid_type, needed_loc, needed_direction)
         needed_energy = temporary_proteins[i].check_energy(needed_loc, acid_type)
         temporary_proteins[i].energy += needed_energy
-        print(temporary_proteins[i])
+        #print(temporary_proteins[i])
 
         beam_list[i] = beam_possibilities[key]
         beam_list[i] = [beam_list[i], key]
         i+=1
-    print(beam_list)
-    print("PROTEINS: ", proteins[0], proteins[1], proteins[2])
-    print("TEMP: ", temporary_proteins[0], temporary_proteins[1], temporary_proteins[2])
+    #print(beam_list)
+    #print("PROTEINS: ", proteins[0], proteins[1], proteins[2])
+    #print("TEMP: ", temporary_proteins[0], temporary_proteins[1], temporary_proteins[2])
 
 
     for i in range(len(beam_list)):
@@ -179,8 +191,8 @@ def next_layer(the_protein, prev_location):
         # location = locations[direction]
         # prev_locs.append(location)
 
-    print("prev: ", prev_locs)
-    print("PROTEINS: ", proteins[0], proteins[1], proteins[2])
+    #print("prev: ", prev_locs)
+    #print("PROTEINS: ", proteins[0], proteins[1], proteins[2])
 
     if proteins[0].length == protein_length:
         return(temporary_proteins)
