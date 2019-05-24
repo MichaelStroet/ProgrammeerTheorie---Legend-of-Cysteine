6c8b38cfb66b34dffc67742c8bb61a18486b1df0
Michael Stroet PC <michael.stroet@hotmail.com>
2019-05-24 18:41:10 +0200

    minimal_sizes comments and renamed "cut size" in main

--- code/visualisations/minimal_sizes.py
+++ code/visualisations/minimal_sizes.py
@@ -7,54 +7,53 @@ import matplotlib.pyplot as plt
 from dict_average import dict_average
 
 def plot_matrix_sizes(matrix_sizes, used_size):
+        '''
+        Draws a plot of the matrix size range per energy
+        '''
 
+        # Dictionaries for the range of matrix sizes per energy
         smallest_matrix_sizes = {}
         average_matrix_sizes = {}
         largest_matrix_sizes = {}
 
+        # Determine the smallest, largest and average matrix size per energy
+        # and add them to the dictionaries at that energy
         for energy, sizes in matrix_sizes.items():
             smallest_matrix_sizes[energy] = min(sizes.keys())
             average_matrix_sizes[energy] = dict_average(sizes)
             largest_matrix_sizes[energy] = max(sizes.keys())
 
-        sorted_smallest_sizes = sorted(smallest_matrix_sizes.items())
-        sorted_average_sizes = sorted(average_matrix_sizes.items())
-        sorted_largest_sizes = sorted(largest_matrix_sizes.items())
+        # Create a list of the energies sorted by their value
+        sorted_energies = [energy for energy in sorted(smallest_matrix_sizes.keys())]
 
-        sorted_energy = []
-        sorted_smallest = []
-        sorted_average = []
-        sorted_largest = []
+        # Turn all dictionaries into lists of their matrix sizes sorted by energy
+        sorted_smallest = [value for energy,value in sorted(smallest_matrix_sizes.items())]
+        sorted_average = [value for energy,value in sorted(average_matrix_sizes.items())]
+        sorted_largest = [value for energy,value in sorted(largest_matrix_sizes.items())]
 
-        for i in range(len(matrix_sizes)):
-            sorted_energy.append(sorted_smallest_sizes[i][0])
-
-            sorted_smallest.append(sorted_smallest_sizes[i][1])
-            sorted_average.append(sorted_average_sizes[i][1])
-            sorted_largest.append(sorted_largest_sizes[i][1])
-
-        x_min = min(sorted_energy)
+        # Minimum and maximum values for the axes
+        x_min = min(sorted_energies)
         x_max = 0
-
         y_min = 0
         y_max = used_size + 1
 
+        # Define the matrix sizes figure
         plt.figure("Matrix sizes per energy", figsize = (6, 6))
 
-        line_width = 2
-        plt.hlines(used_size, x_min, x_max, linewidth = line_width, linestyles = "dashed", color = "black", label = "Matrix used")
-        plt.hlines(2 * used_size / 3., x_min, x_max, linewidth = 2 * line_width / 3., linestyles = "dashed", color = "black")
-        plt.hlines(used_size / 3., x_min, x_max, linewidth = line_width / 3., linestyles = "dashed", color = "black")
+        # Plot a horizontal line for the used matrix size
+        plt.hlines(used_size, x_min, x_max, linewidth = 2, linestyles = "dashed", color = "black", label = "Matrix used")
 
-        plt.plot(sorted_energy, sorted_largest, '-', color = "red", label = "Largest")
-        plt.plot(sorted_energy, sorted_average, '-', color = "blue", label = "Average")
-        plt.plot(sorted_energy, sorted_smallest, '-', color = "red", label = "Smallest")
+        # Plot the range of matrix sizes for each energy
+        plt.plot(sorted_energies, sorted_largest, '-', color = "red", label = "Largest")
+        plt.plot(sorted_energies, sorted_average, '-', color = "blue", label = "Average")
+        plt.plot(sorted_energies, sorted_smallest, '-', color = "red", label = "Smallest")
 
-
+        # Define the x-axis properties
         plt.xlabel("Energy")
         plt.xticks(range(x_min, x_max + 1, 1))
         plt.xlim([x_min, x_max])
 
+        # Define the y-axis properties
         plt.ylabel("Matrix size")
         plt.yticks(range(1, y_max, 2))
         plt.ylim([y_min, y_max])
@@ -62,7 +61,5 @@ def plot_matrix_sizes(matrix_sizes, used_size):
         plt.title("Minimal matrix sizes per energy")
 
         plt.gca().invert_xaxis()
-
         plt.grid(axis = "both")
-
-        plt.legend(loc = "upper right", bbox_to_anchor = (1, 1 - (1. / y_max)))
+        plt.legend(loc = "lower left")
